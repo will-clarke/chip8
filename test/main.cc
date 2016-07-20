03894f42d1412dd708697a0ffeec9e19f1a862a8
William <wmmclarke@gmail.com>
2016-07-20 23:34:46 +0100

    Display opcode Kind of works. Just need to properly xor it, check for collision & also write some decent tests (eg. of % behaviour && in strange positions)

--- test/main.cc
+++ test/main.cc
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "io.cc"
 #include "cpu.cc"
+#include "opcodes.cc"
+#include "../src/cpu.c"
+#include "../src/io.c"
+#include "../src/opcodes.c"
+#include "../src/debug.c"
 
 int main(int argc, char *argv[]){
   ::testing::InitGoogleTest(&argc, argv);
